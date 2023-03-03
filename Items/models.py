d5c5e89b302f512817744079cc754b2c6c8156f6
asushma <animireddysushma2001@gmail.com>
2023-03-03 11:39:48 +0530

    modified test folder

--- Items/models.py
+++ Items/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+	name = models.CharField(max_length=10)
+	phone_number = models.IntegerField(blank=True,null=True)
+	first_name = models.CharField(max_length=10,blank=True,null=True)
+	last_name = models.CharField(max_length=10,blank=True,null=True)
+	e_mail = models.CharField(max_length=100,blank=True,null=True)
+	gender = models.IntegerField(choices=status_choices,blank=True,null=True)
